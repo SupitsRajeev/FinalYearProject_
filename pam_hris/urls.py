3e72a4980788549a159045f2ab8ce842a96fd205
SupitsRajeev <146926341+SupitsRajeev@users.noreply.github.com>
2025-04-13 23:36:08 +0545

    Removed unnecessary files

--- pam_hris/urls.py
+++ pam_hris/urls.py
@@ -21,8 +21,10 @@ from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-     path('', views.home, name='home'),
-     path('dashboard/', views.dashboard, name='dashboard'),
-     path('login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
-     path('logout/', auth_views.LogoutView.as_view(next_page='login'), name='logout'),
+    # path('', views.home, name='home'),  ← temporarily remove or comment this line
+    path('dashboard/', views.dashboard, name='dashboard'),
+    path('login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(next_page='login'), name='logout'),
 ]
+
+
