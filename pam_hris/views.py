54d425c4744483811a2bcb3253e336c257621a3c
SupitsRajeev <146926341+SupitsRajeev@users.noreply.github.com>
2025-04-13 23:36:08 +0545

    Removed unnecessary files

--- pam_hris/views.py
+++ pam_hris/views.py
@@ -1,10 +1,6 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-from .models import User, UserPriviledge, UserPriviledgeGroup
 from django.contrib.auth.decorators import login_required
-
-def home(request):
-    return HttpResponse("Welcome to PAM-HRIS Home")
+from django.shortcuts import render
+from .models import UserPriviledge
 
 @login_required
 def dashboard(request):
@@ -12,12 +8,13 @@ def dashboard(request):
     try:
         priv = UserPriviledge.objects.get(user=user)
         role = priv.priviledge_group.role_level
+        is_hr = user.isHr
+        is_it = user.isIt
 
-        if role == 1:
-            return HttpResponse("Superuser Dashboard")
-        elif role == 2:
-            return HttpResponse("Privileged User Dashboard")
-        else:
-            return HttpResponse("Normal User Dashboard")
+        return render(request, 'dashboards/main_dashboard.html', {
+            'role': role,
+            'is_hr': is_hr,
+            'is_it': is_it,
+        })
     except UserPriviledge.DoesNotExist:
-        return HttpResponse("No privileges assigned to this user.")
+        return render(request, 'dashboards/no_privilege.html')
